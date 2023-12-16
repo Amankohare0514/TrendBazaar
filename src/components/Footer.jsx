ca1567da6af319af7c856932d3dc911d7aab730a
Aman kohare <amankohare@gmail.com>
2023-12-16 11:39:20 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="mb-0 text-center">
+      <footer className="mb-0 text-center border p-3">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
+              <a  href="https://heyamankohare.vercel.app" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Aman kohare</a>
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
+            <a className="text-dark fs-4" href="https://github.com/Amankohare0514" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
           </div>
